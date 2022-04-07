8acf4266ac63bc3460d03df4c53c3fd5f77e74e6
4grobika <39532725+4grobika@users.noreply.github.com>
2022-04-07 21:17:58 +0400

    the end of project

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+def showSignUp():
+    return render_template('VKR_main.html')
